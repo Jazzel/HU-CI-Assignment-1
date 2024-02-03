afc8f66c0927b5d5ec0be0108923a23e025c1b34
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2024-02-03 15:56:44 +0500

    Working on EA

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class EvolutionaryAlgorithm:
     """
     A class representing an evolutionary algorithm.
@@ -17,29 +20,60 @@ class EvolutionaryAlgorithm:
     - run(): Runs the evolutionary algorithm.
     """
 
-    def __init__(self, population_size, mutation_rate, crossover_rate):
+    def __init__(
+        self,
+        population_size: int = 30,
+        no_of_offsprings: int = 10,
+        no_of_generations: int = 50,
+        mutation_rate: float = 0.5,
+        no_of_iterations: int = 10,
+    ) -> None:
         self.population_size = population_size
+        self.no_of_offsprings = no_of_offsprings
+        self.no_of_generations = no_of_generations
         self.mutation_rate = mutation_rate
-        self.crossover_rate = crossover_rate
-        # Add any other necessary attributes
+        self.no_of_iterations = no_of_iterations
 
-    def initialize_population(self):
+        self.selection_scheme = 1
+
+    def initialize_population(self) -> None:
         # Initialize the population with random individuals
-        pass
+        self.population = {i: self.chromosome() for i in range(self.population_size)}
 
-    def evaluate_fitness(self):
-        # Evaluate the fitness of each individual in the population
-        pass
-
-    def selection(self):
+    def selection(self) -> None:
         """
         Performs selection to choose parents for reproduction.
         """
-        pass
+        if self.selection_scheme == 1:
+            self.parents = self.fitness_proportionate_selection()
+        elif self.selection_scheme == 2:
+            self.parents = self.rank_based_selection()
+        elif self.selection_scheme == 3:
+            self.parents = self.binary_tournament_selection()
+        elif self.selection_scheme == 4:
+            self.parents = self.truncation_selection()
+        elif self.selection_scheme == 5:
+            self.parents = self.random_selection()
+        else:
+            print("Invalid selection scheme")
 
     def crossover(self):
         # Perform crossover to create offspring
-        pass
+        parent1, parent2 = self.parents
+
+        chromosome_parent1 = self.population[parent1]
+        chromosome_parent2 = self.population[parent2]
+
+        length = len(chromosome_parent1)
+        start, end = sorted(random.sample(range(length), 2))
+
+        offspring = chromosome_parent1[start:end]
+        remaining_cities = [
+            city for city in chromosome_parent2 if city not in offspring
+        ]
+        offspring.extend(remaining_cities)
+
+        return offspring
 
     def mutation(self):
         # Perform mutation on the offspring
@@ -54,12 +88,28 @@ class EvolutionaryAlgorithm:
         self.initialize_population()
 
         # stoppping condition
-        while not stopping_condition:
-            self.evaluate_fitness()
-            self.selection()
-            self.crossover()
-            self.mutation()
-            self.replace_population()
+        # while not stopping_condition:
+        # self.evaluate_fitness()
+        # self.selection()
+        # self.crossover()
+        # self.mutation()
+        # self.replace_population()
 
         # Return the best individual or other desired output
+        pass
+
+    # Selection schemes
+    def fitness_proportionate_selection(self):
+        pass
+
+    def rank_based_selection(self):
+        pass
+
+    def binary_tournament_selection(self):
+        pass
+
+    def truncation_selection(self):
+        pass
+
+    def random_selection(self):
         pass
---
+++ tsp.py
@@ -0,0 +1,98 @@
+import random
+
+from main import EvolutionaryAlgorithm
+
+
+class TSP(EvolutionaryAlgorithm):
+
+    def __init__(
+        self,
+        filename,
+        selection_scheme,
+        population_size,
+        max_generations,
+    ):
+        super().__init__(
+            selection_scheme,
+            population_size,
+            max_generations,
+        )
+        self.population_size = population_size
+        self.max_generations = max_generations
+        self.filename = filename
+        self.selection_scheme = selection_scheme
+        self.name = ""
+        self.comment1 = ""
+        self.comment2 = ""
+        self.type = ""
+        self.dimension = 0
+        self.edgeWeightType = ""
+        self.nodeCoordSelection = ""
+        self.node_coords = []
+        self.distance_matrix = []
+        self.read_file()
+
+    def read_file(self):
+        with open(self.filename, "r") as f:
+            self.name = f.readline().strip()
+            self.comment1 = f.readline().strip()
+            self.comment2 = f.readline().strip()
+            self.type = f.readline().strip()
+            self.dimension = int(f.readline().strip().split()[2])
+            self.edgeWeightType = f.readline().strip()
+            self.nodeCoordSelection = f.readline().strip()
+
+            line = f.readline().strip()
+            while line != "EOF":
+                self.node_coords.append(list(map(float, line.split())))
+                line = f.readline().strip()
+
+        self.distance_matrix = [
+            [0 for _ in range(self.dimension)] for _ in range(self.dimension)
+        ]
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                self.distance_matrix[i][j] = self.euclidean_distance(
+                    self.node_coords[i], self.node_coords[j]
+                )
+
+    def euclidean_distance(self, p1, p2):
+        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+
+    def chromosome(self) -> list:
+        arr = [i for i in range(self.dimension)]
+        random.shuffle(arr)
+        return arr
+
+    def evaluate_fitness(self, chromosome) -> float:
+        fitness = 0
+        for i in range(self.dimension - 1):
+            fitness += self.distance_matrix[chromosome[i]][chromosome[i + 1]]
+        fitness += self.distance_matrix[chromosome[self.dimension - 1]][chromosome[0]]
+        return fitness
+
+    def compute_population_fitness(self) -> list:
+        fitness_dictionary = {}
+        for individual, chromosome in self.population.items():
+            fitness_dictionary[individual] = self.evaluate_fitness(chromosome)
+
+        self.fitness_dictionary = fitness_dictionary
+        return fitness_dictionary
+
+
+filename = "qa194.tsp"
+selection_scheme = 1
+population_size = 30
+max_generations = 50
+
+tsp = TSP(
+    filename=filename,
+    selection_scheme=selection_scheme,
+    population_size=population_size,
+    max_generations=max_generations,
+)
+
+ch = tsp.chromosome()
+
+print(tsp.initialize_population())
+print(tsp.compute_population_fitness())
